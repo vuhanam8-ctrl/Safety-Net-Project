3bb0977ce8e7c803591294550415b4d139a7b526
Vu Hai Nam <vuhanam8-ctrl@users.noreply.github.com>
2026-09-13 01:26:28 +0700

    Remove canvas swipe guidance

--- sketch.js
+++ sketch.js
@@ -46,7 +46,6 @@ class Ball {
     this.detectFood();
     this.updateSensorPositions();
     this.respondToSensorSignals();
-    this.respondToGuidance();
     this.updateFoodMovement();
     this.updateDispersal();
 
@@ -135,28 +134,6 @@ class Ball {
     if (random(1) < 0.008) {
       this.direction += random(-100, 100);
     }
-  }
-
-  respondToGuidance() {
-    if (this.hasReachedFood || this.isDispersing || guidanceTrail.length === 0) return;
-
-    let closestPoint = null;
-    let closestDistance = Infinity;
-
-    for (const point of guidanceTrail) {
-      const pointDistance = dist(this.x, this.y, point.x, point.y);
-      if (pointDistance < closestDistance) {
-        closestDistance = pointDistance;
-        closestPoint = point;
-      }
-    }
-
-    if (!closestPoint || closestDistance > GUIDANCE_RADIUS) return;
-
-    const targetDirection = atan2(closestPoint.y - this.y, closestPoint.x - this.x);
-    const influence = map(closestDistance, 0, GUIDANCE_RADIUS, 0.075, 0.012, true);
-    this.direction += angleDifference(this.direction, targetDirection) * influence;
-    this.direction += random(-3.5, 3.5);
   }
 
   updateFoodMovement() {
@@ -720,17 +697,12 @@ let heartbeatAudioEnabled = false;
 let nextHeartbeatTime = 0;
 let canvasSoundEnabled = false;
 let ignoreNextDeltaTime = false;
-let guidanceTrail = [];
-let guidanceGestureActive = false;
 
 const STARTING_BALL_COUNT = 250;
 const MINIMUM_FOOD_RESPAWN_DELAY = 180;
 const MAXIMUM_FOOD_RESPAWN_DELAY = 480;
 const PHONE_TONE_START_DELAY = 5000;
 const PHONE_TONE_GAP = 2500;
-const GUIDANCE_RADIUS = 155;
-const GUIDANCE_LIFETIME = 2400;
-const MAX_GUIDANCE_POINTS = 70;
 
 
 
@@ -755,7 +727,6 @@ function setup() {
   const pocketHeight = canvasPocket.clientHeight || 540;
   const mainCanvas = createCanvas(pocketWidth, pocketHeight);
   mainCanvas.parent(canvasPocket);
-  setupCanvasGuidance(mainCanvas.elt);
   angleMode(DEGREES);
 
   createDrawingLayers();
@@ -769,7 +740,6 @@ function setup() {
 }
 
 function draw() {
-  updateGuidanceTrail();
   drawBackground();
   updateSensorLayer();
   updateFoodRespawn();
@@ -788,53 +758,6 @@ function windowResized() {
   recreateDrawingLayers(oldTrails);
   keepFoodInsideCanvas();
 }
-
-function setupCanvasGuidance(canvasElement) {
-  canvasElement.style.touchAction = "none";
-
-  canvasElement.addEventListener("pointerdown", event => {
-    guidanceGestureActive = true;
-    guidanceTrail = [];
-    canvasElement.setPointerCapture(event.pointerId);
-    addGuidancePoint(event, canvasElement);
-  });
-
-  canvasElement.addEventListener("pointermove", event => {
-    if (!guidanceGestureActive) return;
-    addGuidancePoint(event, canvasElement);
-  });
-
-  const endGuidanceGesture = event => {
-    guidanceGestureActive = false;
-    if (canvasElement.hasPointerCapture(event.pointerId)) {
-      canvasElement.releasePointerCapture(event.pointerId);
-    }
-  };
-
-  canvasElement.addEventListener("pointerup", endGuidanceGesture);
-  canvasElement.addEventListener("pointercancel", endGuidanceGesture);
-}
-
-function addGuidancePoint(event, canvasElement) {
-  const bounds = canvasElement.getBoundingClientRect();
-  const canvasX = (event.clientX - bounds.left) * width / bounds.width;
-  const canvasY = (event.clientY - bounds.top) * height / bounds.height;
-
-  guidanceTrail.push({
-    x: constrain(canvasX, 0, width),
-    y: constrain(canvasY, 0, height),
-    createdAt: millis()
-  });
-
-  if (guidanceTrail.length > MAX_GUIDANCE_POINTS) guidanceTrail.shift();
-}
-
-function updateGuidanceTrail() {
-  const cutoff = millis() - GUIDANCE_LIFETIME;
-  guidanceTrail = guidanceTrail.filter(point => point.createdAt >= cutoff);
-}
-
-
 
 // Ball functions
 

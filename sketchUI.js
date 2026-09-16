e4457d62f8d81064b71d5a87999c771e634700b2
Vu Hai Nam <vuhanam8-ctrl@users.noreply.github.com>
2026-09-16 20:25:24 +0700

    Revert "Adapt supplied palette and layout styling"
    This reverts commit 2cf2a234514c1c1baf4ee802f3284aa2c5d51b8f.

--- sketchUI.js
+++ sketchUI.js
@@ -51,10 +51,10 @@ const scrollingAdInstance = new p5(scrollingAdSketch);
 
 const backgroundSketch = (p) => {
   const colors = [
-    [105, 0, 125],
-    [25, 0, 85],
-    [50, 0, 105],
-    [135, 0, 170]
+    [55, 0, 192],
+    [157, 0, 255],
+    [255, 0, 255],
+    [255, 0, 128]
   ];
   const lowResolutionWidth = 400;
   const orbs = [
